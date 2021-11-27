53b4d86685eb4b7c76b6d0624a63349c41284f97
AdriGB <adrigb.zackysh@gmail.com>
2021-11-27 18:13:53 +0100

    feat(scaffolding): implemented project skeleton
    Look readme for more info about this project structure
    
    closes #1

---
+++ android/app/src/main/kotlin/com/example/intersmeet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.intersmeet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) => const MaterialApp();
+}
