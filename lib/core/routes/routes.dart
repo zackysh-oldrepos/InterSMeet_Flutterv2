dae1151d6b481739e487a6a9879085711ef700b2
AdriGB <adrigb.zackysh@gmail.com>
2021-12-02 17:43:53 +0100

    bug(sign-in-view): keyboard cover inputs

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intersmeet/ui/sign_in/sign_in_view.dart';
 import 'package:intersmeet/ui/welcome/welcome_view.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
-    '/': (BuildContext context) => const WelcomeView()
+    '/': (context) => const WelcomeView(),
+    'sign-in': (context) => const SignInView(),
   };
 }
--- lib/ui/welcome/welcome_view.dart
+++ lib/ui/welcome/welcome_view.dart
@@ -6,18 +6,135 @@ class WelcomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to the second screen when tapped.
-          },
-          child: const Text('Launch screen'),
-        ),
+    var logo = Container(
+      alignment: Alignment.topCenter,
+      margin: const EdgeInsets.only(top: 100),
+      child: const Image(
+        image: AssetImage('assets/logo-white.png'),
+        width: 350,
       ),
     );
+
+    var nextButton = Container(
+      alignment: Alignment.bottomCenter,
+      margin: const EdgeInsets.only(bottom: 20.0, left: 15, right: 15),
+      child: ElevatedButton.icon(
+        label: const Text(
+          'Launch screen',
+          style: TextStyle(fontSize: 20),
+        ),
+        icon: const Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.white,
+          size: 24.0,
+        ),
+        onPressed: () {
+          Navigator.pushNamed(context, 'sign-in');
+        },
+        style: ElevatedButton.styleFrom(
+            minimumSize: const Size.fromHeight(40), primary: Colors.black),
+      ),
+    );
+
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Stack(
+            children: [
+              logo,
+              Container(
+                margin: const EdgeInsets.only(top: 350, right: 5, left: 5),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              topLeft: Radius.circular(20)),
+                        ),
+                        padding: const EdgeInsets.only(bottom: 15),
+                        height: 160,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                                flex: 3,
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    "+3k Companies Offering Intership",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                )),
+                            Expanded(
+                                flex: 2,
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  child: const Icon(
+                                    Icons.badge_outlined,
+                                    color: Colors.black,
+                                    size: 54.0,
+                                  ),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(20),
+                              topRight: Radius.circular(20)),
+                        ),
+                        padding: const EdgeInsets.only(bottom: 15),
+                        height: 160,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                                flex: 3,
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    "+3k Companies Offering Intership",
+                                    style: TextStyle(
+                                        fontSize: 24, color: Colors.white),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                )),
+                            Expanded(
+                                flex: 2,
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  child: const Icon(
+                                    Icons.badge_outlined,
+                                    color: Colors.white,
+                                    size: 54.0,
+                                  ),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              nextButton,
+            ],
+          ),
+        ));
   }
 }
+
+/*
+
+
+
+*/
